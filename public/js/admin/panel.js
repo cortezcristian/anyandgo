e1f7befa1ef195364ed989b70ef6ec4c43b65d7c
Cristian Cortez <cortez.cristian@gmail.com>
2014-10-12 17:44:25 -0300

    Dashboard improvements

--- public/js/admin/panel.js
+++ public/js/admin/panel.js
@@ -1,3 +1,37 @@
+'use strict';
 $(document).ready(function(){
     $('#side-menu').metisMenu();
 });
+
+
+/**
+ * @ngdoc overview
+ * @name rdashboardTestApp
+ * @description
+ * # rdashboardTestApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('rdashboardTestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
