5791435c4c7b9e16d22a9567310ad95421c26cf9
Cristian Cortez <cortez.cristian@gmail.com>
2014-10-15 01:16:53 -0300

    Adding travisci config file

--- config/index.js
+++ config/index.js
@@ -7,9 +7,13 @@ switch(process.env.NODE_ENV) {
     case 'production':
         config = require('./config-prod.json');
     break;
+    case 'travisci':
+        config = require('./config-travisci.json');
+    break;
     default:
         config = require('./config-local.json');
         process.env.NODE_ENV = 'development';
+    break;
 }
 
 module.exports = config;
