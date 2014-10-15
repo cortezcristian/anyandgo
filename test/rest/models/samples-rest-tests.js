5285c3adca222dea8fab462336708cb2b8e01b4d
Cristian Cortez <cortez.cristian@gmail.com>
2014-10-15 00:12:23 -0300

    Adding rest test

---
+++ test/rest/models/samples-rest-tests.js
@@ -0,0 +1,84 @@
+// Samles REST API
+// -----------------------------
+
+// Modules Dependencies:
+//  - Assert (http://nodejs.org/api/assert.html)
+//  - SuperAgent (http://visionmedia.github.io/superagent/)
+var assert = require('assert'),
+    config = require('../../../config'),
+    superagent = require('superagent');
+
+// Require basic config files and DB connection
+require('../../../utils/dbconnect');
+
+// Global Variables for the test case
+var Sample, sample, agent, sampleId, d;
+
+// Unit Tests
+describe('REST API Sample', function(){
+    before(function(){
+        // Before all tests
+        Sample = require("../../../models/sample.js");
+        // It show create a new document in the database
+        sample = new Sample({ name: 'sample'+Math.floor((Math.random() * 10) + 1)});
+        sample.save(function(err, doc){
+            sampleId = doc._id;    
+        });
+        // Get domain
+        d = config.app.domain+":"+config.app.port;
+        // Start agent
+        agent = superagent.agent();
+        // Login if necesary
+    });
+
+    describe('Samples REST', function(){
+        it('GET /api/v1/samples', function(done){
+            agent
+              .get(d+'/api/v1/samples')
+              .end(function(res) {
+                  assert.ok(res.ok);
+                  assert.ok(res.body.length>0);
+                  done();
+              });
+        });
+        it('GET /api/v1/samples/count', function(done){
+            agent
+              .get(d+'/api/v1/samples/count')
+              .end(function(res) {
+                  assert.ok(res.ok);
+                  assert.ok(res.body.count > 0);
+                  done();
+              });
+        });
+        it('POST /api/v1/samples', function(done){
+            agent
+              .post(d+'/api/v1/samples')
+              .send({ name: 'Test Creation Sample' })
+              .end(function(res) {
+                  assert.ok(res.ok);
+                  assert.ok(res.body.name === 'Test Creation Sample');
+                  done();
+              });
+        });
+        it('PUT /api/v1/samples/:sampleId', function(done){
+            agent
+              .put(d+'/api/v1/samples/'+sampleId)
+              .send({ name: 'Test Change Sample' })
+              .end(function(res) {
+                  assert.ok(res.ok);
+                  assert.ok(res.body.name === 'Test Change Sample');
+                  done();
+              });
+        });
+        it('DELETE /api/v1/samples/:sampleId', function(done){
+            agent
+              .del(d+'/api/v1/samples/'+sampleId)
+              .end(function(res) {
+                  assert.ok(res.ok);
+                  assert.ok(JSON.stringify(res.body) === '{}');
+                  done();
+              });
+        });
+
+    });
+});
