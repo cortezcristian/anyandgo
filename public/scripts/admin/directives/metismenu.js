25c17363d93bbe6bfaeba06274d381c2173957fc
Cristian Cortez <cortez.cristian@gmail.com>
2014-10-17 02:54:19 -0300

    Adding directive for metismenu

--- public/scripts/admin/directives/metismenu.js
+++ public/scripts/admin/directives/metismenu.js
@@ -9,10 +9,9 @@
 angular.module('anyandgoApp')
   .directive('metismenu', function () {
     return {
-      template: '<div></div>',
-      restrict: 'E',
+      restrict: 'A',
       link: function postLink(scope, element, attrs) {
-        element.text('this is the metismenu directive');
+        $(element).metisMenu(scope.$eval(attrs.toolbarTip));
       }
     };
   });
