595a0e1176d6f84d9edf945d923e596f59d710a7
Cristian Cortez <cortez.cristian@gmail.com>
2015-09-06 16:07:35 -0300

    Changing install script

--- bower_install.js
+++ bower_install.js
@@ -6,29 +6,30 @@ process.on('uncaughtException', function(err) {
       console.log("Exception", err.stack);
 });
 
+console.log('Starting directory: ' + process.cwd());
 // Install for site
 // ./views/layout.jade
 bower.commands
 .install([path.resolve(".")+"/bower.json"])
 .on('end', function (installed) {
     console.log(installed);
+    console.log("Finish Installing Site Components")
+    try {
+      process.chdir(path.resolve(".")+"/public/scripts/admin/");
+      console.log('New directory: ' + process.cwd());
+    }
+    catch (err) {
+      console.log('chdir: ' + err);
+    }
+
+    // Install for admin pandel deps
+    // ./views/layout-admin.jade
+    bower.commands
+    .install([path.resolve(".")+"/bower.json"])
+    //.install([path.resolve(".")+"/public/scripts/admin/bower.json"], { cwd: path.resolve(".")+"/public/scripts/admin/" })
+    .on('end', function (installed) {
+        console.log("Installing admin panel dependencies")
+        console.log(installed);
+    });
 });
 
-console.log('Starting directory: ' + process.cwd());
-try {
-  process.chdir(path.resolve(".")+"/public/scripts/admin/");
-  console.log('New directory: ' + process.cwd());
-}
-catch (err) {
-  console.log('chdir: ' + err);
-}
-
-// Install for admin pandel deps
-// ./views/layout-admin.jade
-bower.commands
-.install([path.resolve(".")+"/bower.json"])
-//.install([path.resolve(".")+"/public/scripts/admin/bower.json"], { cwd: path.resolve(".")+"/public/scripts/admin/" })
-.on('end', function (installed) {
-    console.log("Installing admin panel dependencies")
-    console.log(installed);
-});
